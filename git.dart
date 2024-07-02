20ffb6ac9c345c496ca3f2562de7c6845d1b2582
diya1850 <diyafathimaoruvil@gmail.com>
2024-07-02 19:56:34 +0530

    Initial commit: Add Dart program with name and details

---
+++ git.dart
@@ -0,0 +1,5 @@
+void main() {
+  print('Name: Diya Fathima O');
+  print(
+      'Details: Vice Chairperson of SB IEEE\'24, Bachelor\'s in Artificial Intelligence and Data Science at MES College of Engineering');
+}
